f2accb6f1d0e60096627210139d9820ccde3ffa2
007cod <80967777+007cod@users.noreply.github.com>
2021-03-22 20:51:58 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,26 +1,40 @@
 #include <iostream>
-#include<cstdio>
-const int MAX=1010000;
+
 using namespace std;
-typedef long long LL;
-int n,x,l[MAX],u[MAX],w[MAX];
-LL k[1010];
+int n,m,a,b,map[25][25],dx[8]= {1,1,-1,-1,2,2,-2,-2},dy[8]= {2,-2,2,-2,1,-1,1,-1};
+long long k[25][25];
 int main()
 {
-    cin>>n>>x;
-    for(int i=1;i<=n;i++)
-    {
-        scanf("%d %d %d",&l[i],&w[i],&u[i]);
-        for(int j=x;j>=0;j--)
-                k[j]+=l[i];
-        for(int j=x;j>=u[i];j--)
+    cin>>n>>m>>a>>b;
+    map[a][b]=-1;
+    for(int p=0; p<8; p++)
+        if(a+dx[p]>=0&&b+dy[p]>=0)
         {
-
-            k[j]=max(k[j-u[i]]+w[i]-l[i],k[j]);
-
+            map[a+dx[p]][b+dy[p]]=-1;
         }
+    for(int i=0; i<=n; i++)
+    {
 
+        if(map[i][0]==-1)
+            break;
+        else
+            k[i][0]=1;
     }
-    cout<<k[x]*5;
+    for(int j=0; j<=m; j++)
+    {
+        if(map[0][j]==-1)
+            break;
+        else
+            k[0][j]=1;
+    }
+    for(int i=1; i<=n; i++)
+        for(int j=1; j<=m; j++)
+        {
+            if(map[i][j]==-1)
+                k[i][j]=0;
+            else
+                k[i][j]=k[i-1][j]+k[i][j-1];
+        }
+    cout<<k[n][m];
     return 0;
 }
