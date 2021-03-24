cf1574db8413ed46d2c55be674dd105e346fbb53
cquptpy <81010729+cquptpy@users.noreply.github.com>
2021-03-24 22:37:20 +0800

    Add files via upload
    昨日算法改进 哈希表法

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 class Solution:
-    def twoSum( nums, target):
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[i]+nums[j] == target:
-                    return [i, j]
+    def twoSum( self, nums: List[int], target: int):
+        hashtable = {}
+        for i, num in enumerate(nums):
+            if target - num in hashtable:
+                return [hashtable[target - num], i]
+            hashtable[nums[i]] = i
         return []
-
