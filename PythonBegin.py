416eef0d7536607da5a565ef689d4bb8482b7a48
sleepy678 <73593193+sleepy678@users.noreply.github.com>
2021-03-28 23:32:06 +0800

    Add files via upload

--- PythonBegin.py
+++ PythonBegin.py
@@ -1,1 +1,10 @@
-print('Hello World.')
+favorite_languages = {
+    'jen':'python',
+    'sarah':'c',
+    'edward':'ruby',
+    'phil':'python',
+}#字典定义
+language = favorite_languages['jen'].title()
+lan=favorite_languages.get('your','java')
+print(f"jen's favorite_language is {language}")
+print(f"your favorite_language is {lan}")
