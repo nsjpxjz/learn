74eb87e55d8c748846e9afdb5193d3c8bf7eb0fc
007cod <80967777+007cod@users.noreply.github.com>
2021-04-01 07:47:50 +0800

    Add files via upload

--- wgergerg.py
+++ wgergerg.py
@@ -1,16 +1,11 @@
 import cv2 as cv
 import numpy as np
-img=cv.imread('u=1126872681,3306032964&fm=26&gp=0.jpg')
-cv.imshow('dq',img)
-r,c,h=img.shape
-x=np.zeros((r,c,h,8),dtype=np.uint8)
-for i in range(8):
-    x[:,:,:,i]=2**i
-r=np.zeros((r,c,h,8),dtype=np.uint8)
-for i in range(8):
-    r[:,:,:,i]=cv.bitwise_and(img,x[:,:,:,i])
-    mask=r[:,:,:,i]>0
-    r[mask]=255
-    cv.imshow(str(i),r[:,:,:,i])
+img=cv.imread("u=1126872681,3306032964&fm=26&gp=0.jpg")
+hasv=cv.cvtColor(img, cv.COLOR_BGR2HSV)
+h,s,v=cv.split(hasv)
+v[:,:,]=255
+newhasv=cv.merge([h,s,v])
+cv.imshow('happy',hasv)
+art=cv.cvtColor(newhasv,cv.COLOR_HSV2BGR_FULL)
+cv.imshow('art',art)
 cv.waitKey()
-cv.destroyAllWindows()
