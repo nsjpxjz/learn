efac4664752c6d01dcc743abca2ab0ed4ce118cf
sleepy678 <73593193+sleepy678@users.noreply.github.com>
2021-04-12 20:22:15 +0800

    Add files via upload

--- 练习.cpp
+++ 练习.cpp
@@ -1,24 +1,34 @@
 #include <stdio.h>
-int judge(int a,int m,int n)
-{
-	int i,j;
-	for (i=0;i*m<=a;i++)
-	{
-		for (j=0;j*n<=a;j++)
-		{
-			if (i*m+j*n==a) return 0;
-		}
-	} 
-	return 1;
-}
+#define N 20000
 int main()
 {
-	int m,n,i,j;
-	scanf("%d %d",&m,&n);
-	for (i=m*n; ;i--)
-	{
-		if (judge(i,m,n)) break;//若i能被m，n组合，则停止 
+	int i,j,m,n,sum=0,k,flag,min=0;
+    int A[N]={0};
+    scanf("%d %d",&m,&n);//m为下界,n为上界 
+    for (i=0;i<N;i++)
+    {
+    	A[i]=1;//1暂时标为幸运数 
 	}
-	printf("%d",i);
+    for (i=2;i<=n;i++)//从2开始删 
+    {
+    	if (A[i]==0) continue;
+    	else {
+    	k=1;flag=0;
+    	for (j=2;j<=n;j++)
+    	{
+    		if (A[j]==1) k++;//k标序 
+    		if (A[j]==1&&k%i==0) A[j]=0;
+		}
+	    }
+	}
+	for (i=m+1;i<n;i++)
+	{
+		if (A[i]==1)
+		{
+		sum++;
+		printf("%d ",i);
+	    }
+	}
+	printf("%d",sum);
 	return 0;
 }
